0a1300b2387455ae91fae43a4ba9701bd4a04251
daeun0726 <delee2008@naver.com>
2024-06-04 20:34:51 +0900

    feat : print fizzbuzz
    resolve #4

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,6 +3,6 @@ for i in range(1, 101):
 		print("Fizz", end='')
 	if i % 5 == 0:
 		print("Buzz", end='')
-	# if i % 3 != 0 and i % 5 != 0:
-	# 	print(i, end='')
+	if i % 3 != 0 and i % 5 != 0:
+		print(i, end='')
 	print()
